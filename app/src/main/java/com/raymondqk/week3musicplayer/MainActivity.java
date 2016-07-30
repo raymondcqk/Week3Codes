6ff39cd51ec8e2d68e51129ba7bedc1abbd1413c
Raymond-Chan <raymondcqk@163.com>
2016-07-30 21:50:59 +0800

    Initial commit

---
+++ app/src/main/java/com/raymondqk/week3musicplayer/MainActivity.java
@@ -0,0 +1,16 @@
+package com.raymondqk.week3musicplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 陈其康 raymondchan on 2016/7/30 0030.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
