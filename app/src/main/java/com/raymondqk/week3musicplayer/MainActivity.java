bb139be17b872929dbfeb0606123be8ac81a633b
Raymond-Chan <raymondcqk@163.com>
2016-07-31 22:33:00 +0800

    静态加载Fragment -- 通过 在 activity的布局文件仲加入Fragment标签及在name属性中引用对应的fragment类

--- app/src/main/java/com/raymondqk/week3musicplayer/MainActivity.java
+++ app/src/main/java/com/raymondqk/week3musicplayer/MainActivity.java
@@ -1,6 +1,9 @@
 package com.raymondqk.week3musicplayer;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 /**
@@ -8,9 +11,24 @@ import android.os.Bundle;
  */
 public class MainActivity extends Activity {
 
+    private MyFragment mMyFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fg =  getFragmentManager();
+        FragmentTransaction ft = fg.beginTransaction();
+
+        mMyFragment = new MyFragment();
+        ft.add(R.id.container_fragment, mMyFragment);
+        ft.commit();
+
+        Fragment fragment = fg.findFragmentById(R.id.myFragment);
+        if (fragment instanceof MyFragment){
+            //向下造型
+            MyFragment myFragment = (MyFragment) fragment;
+            myFragment.setText("通过FindFragmentById获得该fragment对象");
+        }
     }
 }
--- app/src/main/java/com/raymondqk/week3musicplayer/MyFragment.java
+++ app/src/main/java/com/raymondqk/week3musicplayer/MyFragment.java
@@ -16,29 +16,41 @@ import android.widget.Toast;
 public class MyFragment extends Fragment {
 
     public static final String TAG = MyFragment.class.getSimpleName();
+    private View mView;
+    private TextView mTv;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG,"MyFragment---onCreate()");
+        //执行顺序 1
     }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle saveInstanceState) {
+        //创建并管理Fragment的视图，通过LayoutInflater解析fragment_item.xml得到一个View
+        // activity将该view呈现到屏幕上
         Log.i(TAG,"MyFragment---onCreateView()");
-        View view = inflater.inflate(R.layout.fragment_item,container,false);
-        TextView tv = (TextView) view.findViewById(R.id.fg_text);
-        Toast.makeText(getActivity(),tv.getText().toString(),Toast.LENGTH_SHORT).show();
-        return view;
+        mView = inflater.inflate(R.layout.fragment_item,container,false);
+        mTv = (TextView) mView.findViewById(R.id.fg_text);
+        Toast.makeText(getActivity(), mTv.getText().toString(),Toast.LENGTH_SHORT).show();
+        return mView;
+        //执行顺序 2
     }
-
     @Override
     public void onPause() {
         super.onPause();
+        Log.i(TAG,"MyFragment---onPause() ");
+        //宿主activity被遮挡或退出时先执行
     }
-
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.i(TAG,"MyFragment---onDestroy() ");
+        //Activity退出时，在onPause后执行
+    }
+
+    public void setText(String s) {
+        mTv.setText(s);
     }
 }
