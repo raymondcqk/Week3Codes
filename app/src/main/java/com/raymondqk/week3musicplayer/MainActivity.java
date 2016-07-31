f1873d42ed78df3050dcc4e31d954ee063e5169a
Raymond-Chan <raymondcqk@163.com>
2016-07-31 23:05:36 +0800

    切换两个Fragment

--- app/src/main/java/com/raymondqk/week3musicplayer/MainActivity.java
+++ app/src/main/java/com/raymondqk/week3musicplayer/MainActivity.java
@@ -1,10 +1,11 @@
 package com.raymondqk.week3musicplayer;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by 陈其康 raymondchan on 2016/7/30 0030.
@@ -12,23 +13,56 @@ import android.os.Bundle;
 public class MainActivity extends Activity {
 
     private MyFragment mMyFragment;
+    private SecondFragment mSecondFragment;
+    private Button mBtn_first;
+    private Button mBtn_second;
+    private View.OnClickListener mListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btn_first:
+                    mFt = mFg.beginTransaction();
+                    mFt.hide(mSecondFragment);
+                    mFt.show(mMyFragment);
+                    mFt.commit();
+                    break;
+                case R.id.btn_second:
+                    mFt = mFg.beginTransaction();
+                    mFt.hide(mMyFragment);
+                    mFt.show(mSecondFragment);
+                    mFt.commit();
+                    break;
+            }
+        }
+    };
+    private FragmentManager mFg;
+    private FragmentTransaction mFt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fg =  getFragmentManager();
-        FragmentTransaction ft = fg.beginTransaction();
+        mFg = getFragmentManager();
+        mFt = mFg.beginTransaction();
 
         mMyFragment = new MyFragment();
-        ft.add(R.id.container_fragment, mMyFragment);
-        ft.commit();
+        mSecondFragment = new SecondFragment();
+        mFt.add(R.id.container_fragment, mSecondFragment);
+        mFt.add(R.id.container_fragment, mMyFragment);
+        mFt.hide(mSecondFragment);
+        mFt.commit();
 
-        Fragment fragment = fg.findFragmentById(R.id.myFragment);
-        if (fragment instanceof MyFragment){
-            //向下造型
-            MyFragment myFragment = (MyFragment) fragment;
-            myFragment.setText("通过FindFragmentById获得该fragment对象");
-        }
+
+        mBtn_first = (Button) findViewById(R.id.btn_first);
+        mBtn_second = (Button) findViewById(R.id.btn_second);
+        mBtn_first.setOnClickListener(mListener);
+        mBtn_second.setOnClickListener(mListener);
+//        Fragment fragment = fg.findFragmentById(R.id.myFragment);
+//        if (fragment instanceof MyFragment){
+//            //向下造型
+//            MyFragment myFragment = (MyFragment) fragment;
+//            myFragment.setText("通过FindFragmentById获得该fragment对象");
+//        }
     }
 }
---
+++ app/src/main/java/com/raymondqk/week3musicplayer/SecondFragment.java
@@ -0,0 +1,40 @@
+package com.raymondqk.week3musicplayer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 陈其康 raymondchan on 2016/7/31 0031.
+ */
+public class SecondFragment extends Fragment {
+
+    public static final String TAG = SecondFragment.class.getSimpleName();
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"Second-onDestroy");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG,"Second-onPause");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second,container,false);
+        return view;
+    }
+}
